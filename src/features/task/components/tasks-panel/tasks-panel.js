9997bb1fc6105a11c3dbc1a9092c1544b38d8643
Cornelius Williams <corneliusw@outlook.com>
2021-09-23 18:51:19 -0500

    refactor: update unstyled button

--- src/features/task/components/tasks-panel/tasks-panel.js
+++ src/features/task/components/tasks-panel/tasks-panel.js
@@ -171,7 +171,7 @@ function TasksPanel({list, tasks}) {
                 <DateText className="date">{today}</DateText>
                 </>
                 }
-                <button type="button" className="options-button button-unstyled" onClick={onOptionsClick}>
+                <button type="button" className="unstyled-button options-button" onClick={onOptionsClick}>
                     <i className="bi bi-three-dots"></i>
                 </button>
                 <PopupMenu/>
