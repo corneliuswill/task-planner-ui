811c70070b4fee9ed8cef39fb256fe028353a67c
Cornelius Williams <corneliusw@outlook.com>
2021-09-26 14:13:00 -0500

    refactor: rename tests

--- src/utils/data-utils.js
+++ src/utils/data-utils.js
@@ -1,3 +1,11 @@
+import { createStore, applyMiddleware } from 'redux';
+
 export function generateUID () {
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+
+export function createDummyStore() {
+    const store = createStore(() => [], {}, applyMiddleware());
+
+    return store;
 }
